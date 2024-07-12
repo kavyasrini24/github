92018fda9383810d7632c62f8f21f2f6716c5044
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-12 13:02:48 +0530

    Create Parse.py

---
+++ Parse.py
@@ -0,0 +1,13 @@
+import unittest
+from src.parser import write_properties_not_in_env
+
+class TestParser(unittest.TestCase):
+    def test_write_properties_not_in_env(self):
+        file_2_difference = {'key3'}
+        write_properties_not_in_env(file_2_difference, 'test_properties-not-in-env.txt')
+        with open('test_properties-not-in-env.txt', 'r') as f2d:
+            lines = f2d.readlines()
+        self.assertIn('key3\n', lines)
+
+if __name__ == "__main__":
+    unittest.main()
