631e7d4ffe3377e875b8b207ac48908725d3768b
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-19 12:26:28 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,14 +1,9 @@
-import xml.etree.ElementTree as ET
+# src/config.py
 
-def append_additional_values(env_file, env_file_op, namefinderinxml, xml_files):
-    splitting = namefinderinxml.find('value').text.split("${")
-    
-    if len(splitting) > 1:
-        for index, splitter in enumerate(splitting):
-            if index > 0:
-                for xml_file in xml_files:
-                    parsed_xml = ET.parse(xml_file)
-                    namefinder = parsed_xml.find(f".//*[name='{splitting[index][:-1]}']")
-                    if namefinder:
-                        env_file.writelines(f"{namefinder.find('name').text}={namefinder.find('value').text}\n")
-                        env_file_op.writelines(f"{namefinder.find('name').text}={namefinder.find('value').text}\n")
+from xml.etree import ElementTree as ET
+
+def load_xml_files(*file_paths):
+    xml_files = {}
+    for file_path in file_paths:
+        xml_files[file_path] = ET.parse(file_path).getroot()
+    return xml_files
