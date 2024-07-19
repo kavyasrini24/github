40011b67b38a98a3de47b76f81d8acb83ffc042f
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-19 16:36:06 +0530

    Update TestParse.py

--- TestParse.py
+++ TestParse.py
@@ -2,6 +2,7 @@ import unittest
 from src.parser import write_properties_not_in_env
 
 class TestParser(unittest.TestCase):
+
     def test_write_properties_not_in_env(self):
         file_2_difference = {'key3'}
         write_properties_not_in_env(file_2_difference, 'test_properties-not-in-env.txt')
@@ -9,5 +10,5 @@ class TestParser(unittest.TestCase):
             lines = f2d.readlines()
         self.assertIn('key3\n', lines)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
