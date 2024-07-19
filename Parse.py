9a3c3add3510fc2e5f84b6b52c5b28fa7ffcd220
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-19 16:30:58 +0530

    Update Parse.py

--- Parse.py
+++ Parse.py
@@ -1,23 +1,3 @@
-# src/parser.py
-
-def append_additional_values(env_file, env_file_op, namefinderinxml, xml_files):
-    splitting = namefinderinxml.find('value').text.split("${")
-    if len(splitting) > 1:
-        for index, splitter in enumerate(splitting):
-            if index > 0:
-                element_found = False
-                for xml_name in ['cbc-service-cbc-osb-dev01.xml', 'cbc-service-cbc-osb-common.xml', 'cbc-osb-dev01.xml', 'new-file.xml']:
-                    devxml = xml_files.get(xml_name)
-                    if devxml is None:
-                        print(f"Warning: {xml_name} not found in xml_files")
-                        continue
-
-                    namefinderindevxml = devxml.find(f".//*[name='{splitting[index][:-1]}']")
-                    if namefinderindevxml:
-                        env_file.write(f"{namefinderindevxml.find('name').text}={namefinderindevxml.find('value').text}\n")
-                        env_file_op.write(f"{namefinderindevxml.find('name').text}={namefinderindevxml.find('value').text}\n")
-                        element_found = True
-                        break
-                
-                if not element_found:
-                    print(f"Warning: No element with name '{splitting[index][:-1]}' found in any XML files")
+def write_properties_not_in_env(file_2_difference, output_file_path):
+    with open(output_file_path, mode='wt', encoding='utf-8') as f2d:
+        f2d.write('\n'.join(str(line) for line in file_2_difference))
