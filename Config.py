c60fb586b221adde77b64d94a99c8126ada010ab
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-19 16:29:12 +0530

    Update Config.pyrf

--- Config.py
+++ Config.py
@@ -1,9 +1,11 @@
-# src/config.py
-
+import os
 from xml.etree import ElementTree as ET
 
 def load_xml_files(*file_paths):
     xml_files = {}
     for file_path in file_paths:
-        xml_files[file_path] = ET.parse(file_path).getroot()
+        if os.path.exists(file_path):
+            xml_files[file_path] = ET.parse(file_path).getroot()
+        else:
+            print(f"Warning: {file_path} does not exist")
     return xml_files
