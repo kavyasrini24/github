057b9c71516aad78e2a4038f4cd673b8dd076e4d
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-19 16:34:54 +0530

    Update Test module.py

--- Test module.py
+++ Test module.py
@@ -2,6 +2,7 @@ import unittest
 from src.module import get_keywords_from_file, find_differences
 
 class TestModule(unittest.TestCase):
+
     def test_get_keywords_from_file(self):
         keywords = get_keywords_from_file('test_application.properties')
         self.assertEqual(keywords, ['keyword1', 'keyword2'])
@@ -13,5 +14,5 @@ class TestModule(unittest.TestCase):
         self.assertEqual(file_1_difference, {'key3'})
         self.assertEqual(file_2_difference, {'key1'})
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
